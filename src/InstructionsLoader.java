08610c70c773e507e92a99bb6b4c77527186121b
Hilary Ovares <hil.ov9@gmail.com>
2018-11-18 17:53:56 -0600

    memoria de instrucciones

--- src/InstructionsLoader.java
+++ src/InstructionsLoader.java
@@ -1,8 +1,15 @@
+import javax.swing.text.html.HTMLDocument;
+import java.io.BufferedReader;
+import java.io.*;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 
 public class InstructionsLoader {
 
-    private final String hilillo0 = "hilillo_0";
+    private final String hilillo0 = "hilillo_0.txt";
     private final String hilillo1 = "hilillo_1";
     private final String hilillo2 = "hilillo_2";
     private final String hilillo3 = "hilillo_3";
@@ -20,9 +27,45 @@ public class InstructionsLoader {
      *      true si se pudieron cargar los archivos
      *      false si no se pudieron cargar
      */
-    public static boolean loadInstructions(ArrayList<Integer> instructionsMemory){
+    public static boolean loadInstructions(ArrayList<Integer> instructionsMemory) {
 
+        String path = "assets";
+        Scanner s0;
+
+        File f = new File(path);
+
+        if (f.isDirectory()) {
+
+            File[] arr_cont = f.listFiles();
+            int size = arr_cont.length;
+
+            for (int i = 0; i < size; i++) {
+
+                if (arr_cont[i].isFile()) {
+                    try {
+                        s0 = new Scanner(new File(String.valueOf(arr_cont[i])));
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        return false;
+                    }
+
+                    while (s0.hasNext()) {
+                        instructionsMemory.add(s0.nextInt());
+                    }
+                    s0.close();
+                }
+            }
+            /*Imprimir memoria de instrucciones
+            Iterator<Integer> it = instructionsMemory.iterator();
+            while (it.hasNext()) {
+                int num = it.next();
+                System.out.println(num);
+            }
+            */
+        } else {
+            System.err.println("El path NO es válido!");
+            return false;
+        }
         return true;
     }
-
-}
+ }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         new Main().run();
     }
 
