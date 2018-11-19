3a8954922e16c62631ba20067e3426e3ac03dba5
Jose Mesen Campos <b34252@ecci.ucr.ac.cr>
2018-11-19 16:55:27 -0600

    Se comienza a implementar la etapa EX

--- src/EX.java
+++ src/EX.java
@@ -11,4 +11,37 @@ public class EX implements Runnable {
     public void run(){
         System.out.println("EX is running");
     }
+
+    private int processInstruction() {
+        int operationCode = idEx.ir.getOperationCode();
+        int result = -1;
+
+        switch (operationCode) {
+            case 19: // addi
+                result = idEx.a + idEx.imm;
+                break;
+
+            case 71: // add
+                result = idEx.a + idEx.b;
+                break;
+
+            case 83: // sub
+                result = idEx.a - idEx.b;
+                break;
+
+            case 72: // mul
+                result = idEx.a * idEx.b;
+                break;
+
+            case 56: // div
+                result = idEx.a / idEx.b;
+                break;
+
+            case 5: case 37:// lw & sw
+                result = idEx.a + idEx.imm;
+                break;
+        }
+
+        return result;
+    }
 }
